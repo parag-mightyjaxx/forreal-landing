5f144885b0a4038be34437f0295d1a61a1e72ee7
Parag Bhatnagar <parag@mightyjaxx.com>
2020-05-09 13:25:54 +0800

    styling changes

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -7,7 +7,73 @@ import HeroSection from "../components/heroSection"
 const PrivacyPolicy = () => (
   <Layout>
     <SEO title="Mighty Proof" />
-    <h3>Help us create a better Forreal Technology for you.</h3>
+    <div className="text-wall">
+    <h4>Privacy Policy</h4>
+    <p>Mighty Jaxx built the Forreal Technology app as a Free app. This Service is provided by Mighty Jaxx at no cost and is intended for use as is.</p>
+    <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</p>
+    <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. we will not use or share your information with anyone except as described in this Privacy Policy.</p>
+    <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Forreal Technology unless otherwise defined in this Privacy Policy.</p>
+ 
+    <h4>Information Collection and Use</h4>
+    <p>For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to name, email address, telephone number. The information that we request will be retained by us and used as described in this privacy policy.</p>
+    <p>The app does use third party services that may collect information used to identify you.</p>
+    <p>Link to privacy policy of third party service providers used by the app</p>
+    <ul>
+      <li>Google Play Services</li>
+      <li>Facebook</li>
+    </ul>
+
+    <h4>Customer relations</h4>
+    <p>In order for a customer relations representative to respond to your questions or concerns, we may require certain personal information such as the aforementioned. This information is used to answer your questions as completely and thoroughly as possible.</p>
+
+    <h4>Log Data</h4>
+    <p>we want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.</p>
+
+    <h4>Cookies</h4>
+    <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.</p>
+    <p>This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+
+    <h4>Service Providers</h4>
+    <p>We may employ third-party companies and individuals due to the following reasons:</p>
+    <ul>
+      <li>To facilitate our Service;</li>
+      <li>To provide the Service on our behalf;</li>
+      <li>To perform Service-related services; or</li>
+      <li>To assist us in analyzing how our Service is used.</li>
+    </ul>
+    <p>we want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+
+    <h4>Marketing</h4>
+    <p>By providing any personal information to us, you give us permission to email you for marketing purposes.</p>
+    <p>We will only send you emails which may be of interest to you - for example information about new releases or discounts.
+If you do not want us to send any marketing material to you, you can decide to opt-out of these types of marketing material at any time by changing the settings in your profile or by following the unsubscribe link at the bottom of any email we send to you.</p>
+    
+    <p>
+      <strong>Storing your information and keeping it up to date:</strong><br/>
+      When you share your personal information with Forreal, you will always have access to this information. Where you store a "Profile" on the mobile app, you can amend details stored in your account yourself. If you have any issues, please drop us an email at support@forreal.technology.
+    </p>
+    <p>
+      <strong>Change of personal information? Let us know:</strong><br/>
+      Once you place an order, you log in to update your account contact number or address any time. If you wish to change the shipping details on your order, please drop us an email at support@forreal.technology.
+    </p>
+    <p>
+      If you would like us to update your personal information, remove your name from our mailing list, or if you have any questions about our privacy policy or your personal information we have on record, please contact us through our online request form here or email us at support@forreal.technology.
+    </p>
+
+    <h4>Security</h4>
+    <p>we value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+  
+    <h4>Links to Other Sites</h4>
+    <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. we have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+
+    <h4>Changes to This Privacy Policy</h4>
+    <p>we may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. we will notify you of any changes by posting the new Privacy Policy on this page. This policy is effective as of 2020-05-07</p>
+
+    <h4>Contact Us</h4>
+    <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at support@forreal.technology</p>
+    <p>We will respond to most questions or concerns within 5-7 working days.</p>
+    <p>By using this site, you consent to the above privacy policy.</p>
+    </div>
   </Layout>
 )
 
